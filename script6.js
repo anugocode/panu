dcd8856a48bcd1d7c70e6164a7f48210332c06f6
anugocode <panushka792@gmail.com>
2026-06-22 10:39:26 +0530

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,15 @@
+function add(value)
+{
+    document.getElementById("display").value += value;
+}
+
+function calculate()
+{
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+}
+
+function clearDisplay()
+{
+    document.getElementById("display").value = "";
+}
